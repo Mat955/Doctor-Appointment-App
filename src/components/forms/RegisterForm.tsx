29f124efbe22fbb14c039519108ceb211b0636ef
Mateusz Lewartowski <lewar2013c@gmail.com>
2025-01-22 20:28:15 +0100

    Enhance form functionality with new components and dependencies
    - Added `DatePicker` support in `CustomFormField` for date selection.
    - Introduced `RadioGroup` for gender selection in `RegisterForm` using new `@radix-ui/react-radio-group` component.
    - Implemented `Select` component for dropdown functionality with `@radix-ui/react-select`.
    - Updated `package.json` and `package-lock.json` to include new dependencies: `react-datepicker`, `@radix-ui/react-radio-group`, and `@radix-ui/react-select`.
    - Created a new `constants` file for gender options.
    - Refactored form components to improve user experience and maintainability.

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -3,13 +3,16 @@
 import {zodResolver} from "@hookform/resolvers/zod";
 import {useForm} from "react-hook-form";
 import {z} from "zod";
-import {Form} from "@/components/ui/form";
+import {Form, FormControl} from "@/components/ui/form";
 import CustomFormField, {FormFieldType} from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
 import {useState} from "react";
 import {userFormValidation} from "../../lib/validation";
 import {useRouter} from "next/navigation";
 import {createUser} from "../../lib/actions/patient.actions";
+import {RadioGroup, RadioGroupItem} from "../ui/radio-group";
+import {GenderOptions} from "../../../constants";
+import {Label} from "../ui/label";
 
 const RegisterForm = ({user}: {user: User}) => {
   const router = useRouter();
@@ -44,22 +47,80 @@ const RegisterForm = ({user}: {user: User}) => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6 flex-1'>
-        <section className='mb-12 space-y-4'>
-          <h1 className='header'>Hi there 👋</h1>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='space-y-12 flex-1'
+      >
+        <section className='space-y-4'>
+          <h1 className='header'>Welcome 👋</h1>
           <p className='text-dark-700'>
-            Thank you for checking out our project! We re excited to have you
-            onboard.
+            Let us get more information about you to help us serve you better.
           </p>
+        </section>
+        <section className='space-y-6'>
+          <div className='mb-9 space-y-1'>
+            <h2 className='sub-header'>Personal Information</h2>
+          </div>
         </section>
         <CustomFormField
           fieldType={FormFieldType.INPUT}
           control={form.control}
-          name='name'
-          label='Full Name'
+          name='Full Name'
           iconSrc='/assets/icons/user.svg'
           iconAlt='user'
         />
+        <div className='flex flex-col gap-6 xl:flex-row'>
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name='email'
+            label='Email'
+            placeholder='example@example.com'
+            iconSrc='/assets/icons/email.svg'
+            iconAlt='email'
+          />
+          <CustomFormField
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name='phone'
+            label='Phone Number'
+            placeholder='+(country code) 123 456 789'
+          />
+        </div>
+        <div className='flex flex-col gap-6 xl:flex-row'>
+          <CustomFormField
+            fieldType={FormFieldType.DATE_PICKER}
+            control={form.control}
+            name='birthDate'
+            label='Date of birth'
+          />
+          <CustomFormField
+            fieldType={FormFieldType.SKELETON}
+            control={form.control}
+            name='gender'
+            label='Gender'
+            renderSkeleton={(field) => (
+              <FormControl>
+                <RadioGroup
+                  className='flex h-11 gap-6 xl:justify-between'
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  {GenderOptions.map((option) => (
+                    <div key={option} className='radio-group'>
+                      <RadioGroupItem value={option} id={option} />
+                      <Label htmlFor={option} className='cursor-pointer'>
+                        {option}
+                      </Label>
+                    </div>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            )}
+          />
+        </div>
+        <div className='flex flex-col gap-6 xl:flex-row'></div>
+        <div className='flex flex-col gap-6 xl:flex-row'></div>
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
